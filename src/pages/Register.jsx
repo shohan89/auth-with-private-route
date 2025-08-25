ad6844c966b201011073c0a59a6b9a776b271e19
shohan89 <maffin.killer007@gmail.com>
2025-08-25 23:07:25 +0600

    add event handler to handle register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router";
 
 const Register = () => {
+    const handleRegister = e => {
+        e.preventDefault(); //? to prevent page reload
+        const form = e.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(name, email, password);
+    }
     return (
         <div className="container w-96 mx-auto p-4">
             <h2 className="text-2xl font-bold text-center mb-4">Register Now!</h2>
             <hr />
-            <div className="mt-4">
+            <form onSubmit={handleRegister} className="mt-4">
                 <fieldset className="fieldset">
                     <label className="label">Name</label>
                     <input name="name" type="text" className="input w-full" placeholder="Name" />
@@ -16,7 +24,7 @@ const Register = () => {
                     <button className="btn btn-primary mt-4">Login</button>
                 </fieldset>
                 <p>Already have account? Please <span><Link className="text-primary" to='/login'>Login</Link></span></p>
-            </div>
+            </form>
         </div>
     );
 };
