0144e036492ef02ba600bebbda150ae7ab70741b
shohan89 <maffin.killer007@gmail.com>
2025-08-25 23:05:23 +0600

    add event handler to handle login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,21 +1,28 @@
 import { Link } from "react-router";
 
 const Login = () => {
+    const handleLogin = e => {
+        e.preventDefault(); //? to prevent page reload
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
     return (
         <div className="container w-96 mx-auto p-4">
             <h2 className="text-2xl font-bold text-center mb-4">Login Now!</h2>
             <hr />
-            <div className="mt-4">
+            <form onSubmit={handleLogin} className="mt-4">
                 <fieldset className="fieldset">
                     <label className="label">Email</label>
-                    <input type="email" className="input w-full" placeholder="Email" />
+                    <input name="email" type="email" className="input w-full" placeholder="Email" />
                     <label className="label">Password</label>
-                    <input type="password" className="input w-full" placeholder="Password" />
+                    <input name="password" type="password" className="input w-full" placeholder="Password" />
                     <div><a className="link link-hover">Forgot password?</a></div>
                     <button className="btn btn-primary mt-4">Login</button>
                 </fieldset>
                 <p>Don't have account? Please <span><Link className="text-primary" to='/register'>Register</Link></span></p>
-            </div>
+            </form>
         </div>
     );
 };
