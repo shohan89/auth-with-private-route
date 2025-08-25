f034cc867f31836f005b1160d695a1f01b338cb3
Sharoar Hossain <maffin.killer007@gmail.com>
2025-08-25 19:08:18 +0500

    create main pages and set there routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import './index.css'
+import Root from './layouts/Root'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 // create router
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>This is home.</div>
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: '/login',
+        element: <Login />
+      },
+      {
+        path: '/register',
+        element: <Register />
+      }
+    ]
   }
 ])
 
